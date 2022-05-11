2a20ce9519ba2c9d1a45c60f620905c545a14c5a
harishkumarforever <h.nunavath@dev2prod.com>
2022-05-11 23:31:55 +0530

    ok5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-// import AddVisibility from "./components/AddVisibility/AddVisibility";
+import AddVisibility from "./components/AddVisibility/AddVisibility";
 import Header from "./components/Header/Header";
 import "./App.scss";
-// import TimeManagement from "./components/TimeManagement/TimeManagement";
-// import TaskManager from "./components/TaskManager/TaskManager";
-// import Kanban from "./components/kanban/Kanban";
-// import CreateShare from "./components/CreateShare/CreateShare";
-// import Testimonial from "./components/Testimonial/Testimonial";
-// import Footer from "./components/Footer/Footer";
+import TimeManagement from "./components/TimeManagement/TimeManagement";
+import TaskManager from "./components/TaskManager/TaskManager";
+import Kanban from "./components/kanban/Kanban";
+import CreateShare from "./components/CreateShare/CreateShare";
+import Testimonial from "./components/Testimonial/Testimonial";
+import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <div className="App">
       <Header />
-      {/*  <AddVisibility />
+      <AddVisibility />
       <TimeManagement />
       <TaskManager />
       <Kanban />
       <CreateShare />
       <Testimonial />
-      <Footer /> */}
-      <h1>hhhhhhhhhhhh</h1>
+      <Footer />
     </div>
   );
 };
